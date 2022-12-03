d6be69cf5f13299bf8a7a5cc4483e62282e0d90e
Gaizgrol <gabrielizotongo@gmail.com>
2022-12-02 23:15:59 -0300

    ♻️ Reorganização de arquivos

---
+++ src/test/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "Application.hpp"
+
+auto main() -> int {
+    using namespace cbg;
+    using namespace std;
+
+    Application app{};
+
+    auto win1 = app.createWindow("Janela 1", 320, 240);
+    auto win2 = app.createWindow("Janela 2", 320, 240);
+
+    
+    app.getWindow(win1)->addListener(RESIZE, [](WindowEvent e) {
+        cout << "Janela " << e.window->id << " foi redimensionada!" << endl;
+    });
+
+    app.getWindow(win2)->addListener(RESIZE, [](WindowEvent e) {
+        auto& [width, height] = std::get<WindowResizeEvent>(e.details);
+        cout << "Tamanho da janela " << e.window->id << ": " << width << " x " << height << endl;
+    });
+
+    while (true) {
+        app.main();
+    }
+
+    return 0;
+}
